8ba69dabec098de8fff3db9ba3109acb25f82f9b
Unknown <manuel_ruiz27@live.com>
2018-10-01 06:00:38 -0500

    Se agrega el cambio y uso del timer

---
+++ Tiempo.java
@@ -0,0 +1,29 @@
+import greenfoot.*;
+
+public class Tiempo  extends Actor
+{
+    private SimpleTimer timer;
+    private Counter contador;
+    public Tiempo()
+    {
+        timer=new SimpleTimer();
+        contador= new Counter("Tiempo");
+        contador.setValue(60);
+    }
+     //Se ejecuta al momento de agregar el objeto al munod 
+    protected void addedToWorld(World world)
+    {
+        world.addObject(contador,60,20);
+    }
+    public void act()
+    {
+        if(timer.millisElapsed()>1000){
+           contador.setValue(contador.getValue()-1);
+           if(contador.getVAlue()==0){
+               
+            }
+           timer.mark();
+           
+        }
+    }
+}
