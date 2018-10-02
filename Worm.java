ef7ca57abc139d7b310029a4d50ad4ac2aa01962
Unknown <manuel_ruiz27@live.com>
2018-10-02 04:58:36 -0500

    cambio del numero de gusanos

--- Worm.java
+++ Worm.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.Random;
 /**
  * Write a description of class Worm here.
  * 
@@ -12,7 +12,19 @@ public class Worm extends Actor
      * Act - do whatever the Worm wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public Worm(int cantidad)
+    {  
+        Worm gusano; 
+        for(int i=0;i<cantidad;i++)
+        {
+            int n=600;
+            int x,y;
+            x=(int)(Math.random()*n) + 1;
+            y=(int)(Math.random()*n) + 1;
+            world.addObject(gusano,x,y);
+        }
+    }
+     public void act() 
     {
         // Add your action code here.
     }    
